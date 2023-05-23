584faad827564b8aa64e22b342bdf93309c10aec
Aleksandar Obreshkov <42234192+aleksandarObreshkov@users.noreply.github.com>
2023-05-23 10:15:10 +0300

    Split a large image into sections (#2)

--- tumor_tissue_recognition.py
+++ tumor_tissue_recognition.py
@@ -6,14 +6,17 @@ from tensorflow import keras
 from datasets import dirs
 
 
-def get_single_image_prediction(alexnet_model, image_path):
+def get_multiple_image_prediction(alexnet_model, image_path):
+    batch = os.listdir(f'{image_path}\\cat')
     training_data_gen = tf.keras.preprocessing.image.ImageDataGenerator().flow_from_directory(directory=image_path,
                                                             batch_size=1)
-    
-    res_from_single_image = alexnet_model(training_data_gen.next()[0])
-    print(res_from_single_image)
-    index = np.array(res_from_single_image).argmax()
-    return dirs[index]
+    result = []
+    for _ in batch:
+        res_from_single_image = alexnet_model(training_data_gen.next()[0])
+        print(res_from_single_image)
+        index = np.array(res_from_single_image).argmax()
+        result.append(dirs[index])
+    return result
 
 
 if __name__ == '__main__':
@@ -26,5 +29,5 @@ if __name__ == '__main__':
         print("Loading Alexnet from memory")
         alexnet_model = keras.models.load_model(alexnet.model_name)
 
-    print(get_single_image_prediction(alexnet_model, "C:\\Users\\aleks\\Desktop\\image"))
+    print(get_multiple_image_prediction(alexnet_model, "C:\\Users\\aleks\\Desktop\\image\\testing"))
     
