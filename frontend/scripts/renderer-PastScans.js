735dc39f84515e4186e3128a5d77e17ee2716bb7
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2023-06-18 15:17:34 +0300

    Add separate window for displaying past scans

--- frontend/scripts/renderer-PastScans.js
+++ frontend/scripts/renderer-PastScans.js
@@ -1,22 +1,31 @@
 const backButton = document.getElementById('backButton')
+const ROOT_DIR = "C:\\Users\\aleks\\Projects\\IDC_Finder\\past_scans"
 
-backButton.addEventListener('click', ()=> {
-    window.electronAPI.changeView('pages/index.html');
+backButton.addEventListener('click', () => {
+    window.electronAPI.changeView('pages/index.html', null);
 })
 
 window.addEventListener('load', () => {
-    displayImages("C:\\Users\\aleks\\Desktop\\image\\testing")
+    getPastScans(ROOT_DIR)
 })
 
-async function displayImages(rootDir) {
-    const receivedImages = await window.electronAPI.getImages(rootDir)
-    const imageHolder = document.getElementById('imageHolder')
-    for (let imagePath of receivedImages) {
-        const image = document.createElement('img')
-        let fullImagePath = rootDir+'\\'+imagePath
-        console.log(fullImagePath)
-        image.setAttribute('src', fullImagePath)
-        imageHolder.appendChild(image)
-    }
+async function getPastScans(rootDir) {
+    const receivedScanTimestamps = await window.electronAPI.getImages(rootDir)
+    const pastScansHolder = document.getElementById('pastScansHolder')
+    receivedScanTimestamps.forEach((scans) => {
+        pastScansHolder.appendChild(createNewTileForImage(scans))
+    })
+}
+
+function createNewTileForImage(scanTimestamp) {
+    const previousScanHolder = document.createElement('div')
+    previousScanHolder.addEventListener('click', () => showScan(scanTimestamp))
+    previousScanHolder.classList.add('scan')
+    previousScanHolder.innerText += scanTimestamp
+    return previousScanHolder
+}
+
+function showScan(timestamp) {
+    window.electronAPI.openNewWindow('pages/singleScan.html', [timestamp]);
 }
 
