65f64bb251b28ccfa1672e21d54de1913ec2fee9
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2023-06-25 09:30:26 +0300

    Load the NN only once

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,16 @@
-import os
-import alexnet
 import utils
-from tensorflow import keras
 import image_processing
 
 
-def scan(image_path):
+def scan(image_path, model):
     curr_scan_dir = utils.make_new_dir()
-    utils.copy_original_in_scan_dir(image_path, curr_scan_dir)
-    original_image_path = f"{curr_scan_dir}\\{utils.extract_last_element_from_path(image_path)}"
-    
-    alexnet_model: keras.models.Model
-
-    if alexnet.model_name not in os.listdir(os.curdir):
-        print("Creating Alexnet model")
-        alexnet_model = alexnet.train_model()
-    else:
-        print("Loading Alexnet from memory")
-        alexnet_model = keras.models.load_model(alexnet.model_name)
+    original_image_path = copy_original_to_scan_dir(image_path, curr_scan_dir)
 
     validated_image_path = f"{curr_scan_dir}\\merged.png"
     filter_path = f"{curr_scan_dir}\\filtered.png"
     result_path = f'{curr_scan_dir}\\result.png'
 
-    image_processing.analyze_image(alexnet_model, original_image_path, validated_image_path)
+    image_processing.analyze_image(model, original_image_path, validated_image_path)
     
     tumor_mask = image_processing.get_tumor_mask(validated_image_path)
     tumor_mask.save(filter_path)
@@ -34,3 +21,9 @@ def scan(image_path):
     image_processing.outline_tumors(filter_path, original_image_path, result_path)
     utils.change_file_extension(original_image_path)
     return curr_scan_dir
+
+
+def copy_original_to_scan_dir(image_path, scan_dir):
+    utils.copy_original_in_scan_dir(image_path, scan_dir)
+    original_image_path = f"{scan_dir}\\{utils.extract_last_element_from_path(image_path)}"
+    return original_image_path
--- backend/server.py
+++ backend/server.py
@@ -2,14 +2,25 @@ from flask import Flask, request, Response
 import main
 import os
 import utils
+import alexnet
+from tensorflow import keras
 
 app = Flask(__name__)
 # add env reading and setting
 
+alexnet_model: keras.models.Model
+
+if alexnet.model_name not in os.listdir(os.curdir):
+    print("Creating Alexnet model")
+    alexnet_model = alexnet.train_model()
+else:
+    print("Loading Alexnet from memory")
+    alexnet_model = keras.models.load_model(alexnet.model_name)
+
 @app.route('/scan', methods=['POST'])
 def scan_image():
     image_path = request.get_json()
-    curr_scan_path = main.scan(image_path[0])
+    curr_scan_path = main.scan(image_path[0], alexnet_model)
     curr_scan_path = utils.extract_last_element_from_path(curr_scan_path)
     return Response(curr_scan_path)
 
