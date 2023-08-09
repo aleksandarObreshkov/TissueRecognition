63f46deaa63c5964af62eefd790421493e90fbf2
Aleksandar Obreshkov <aleksandar.obreshkov1@gmail.com>
2023-08-09 15:34:00 +0300

    Remove probability map

--- backend/main.py
+++ backend/main.py
@@ -21,19 +21,19 @@ def scan(original_image_path, curr_scan_dir, model):
     try:
         validated_image_path = f"{curr_scan_dir}\\merged.png"
         result_path = f'{curr_scan_dir}\\result.png'
-        probability_map_path = f'{curr_scan_dir}\\map.png'
+        #probability_map_path = f'{curr_scan_dir}\\map.png'
 
         dims, image_name = svs.split_svs(original_image_path)
         patches, all_filenames = svs.read_svs_patches(image_name)
         results_arr = svs.run_patches_in_nn(patches, model)
-        result_probability_map = svs.merge_scan_arr(dims, all_filenames,results_arr,image_name)
+        #result_probability_map = svs.merge_scan_arr(dims, all_filenames,results_arr,image_name)
 
         name_to_image_map = zip(all_filenames, patches)
         print(f"Dimensions are {dims}")
 
         image_shape = (math.ceil(dims[1]/50)*50, math.ceil(dims[0]/50)*50, 3)
         original_merged, result_tint = merger.merge(name_to_image_map, 50, image_shape, image_name, results_arr)
-        Image.fromarray(result_probability_map).save(probability_map_path)
+        #Image.fromarray(result_probability_map).save(probability_map_path)
 
 
         Image.fromarray(original_merged).save(validated_image_path)
